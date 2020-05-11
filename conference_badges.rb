1680ef9c460680fd980aa0fec4e0a6c8912753ee
Morgan Collado <morgan.collado@gmail.com>
2020-05-11 22:48:08 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+
+
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end 
+
+def batch_badge_creator(speakers)
+  speakers.map do |name|
+     badge_maker(name)
+  end
+end 
+
+def assign_rooms(speakers)
+  speakers.each_with_index.map do |name, index|
+     "Hello, #{name}! You'll be assigned to room #{index + 1}!"
+  end 
+end 
+
+def printer(attendees)
+  batch_badge_creator(attendees).map do |sentence|
+    puts sentence
+  end 
+  assign_rooms(attendees).map do |sentence|
+    puts sentence
+  end 
+end 
