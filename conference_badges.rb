6294a5ed2ff63e176700e297d41499debdb7dfc8
Morgan Collado <morgan.collado@gmail.com>
2020-05-11 22:21:48 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,15 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end 
+
+def batch_badge_creator(speakers)
+  speakers.map do |name|
+     badge_maker(name)
+  end
+end 
+
+def assign_rooms(speakers)
+  speakers.each_with_index do |name, index|
+    return "Hello, #{name}! You'll be assinged to room #{index}!"
+  end 
+end 
