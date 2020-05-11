afc7e464cf6bce8107a6fbb1f9086b456433755c
Morgan Collado <morgan.collado@gmail.com>
2020-05-11 22:27:45 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,6 +10,6 @@ end
 
 def assign_rooms(speakers)
   speakers.each_with_index do |name, index|
-    return "Hello, #{name}! You'll be assinged to room #{index}!"
+    return "Hello, #{name}! You'll be assinged to room #{index + 1}!"
   end 
 end 
